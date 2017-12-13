a0770e0c13b3f9cb0d4b37eacb03a4102bb47fc2
Danny Chang <dannychachang@gmail.com>
2017-12-13 14:06:50 -0500

    Exercise 0 warmup

---
+++ exercise1.rb
@@ -0,0 +1,39 @@
+# Exercise 0 --------------------------------------------------------
+
+# Colors
+
+fav_colors = ["red", "yellow", "purple", "blue"]
+
+# Age
+
+age = [27, 23, 26, 18]
+
+# Coin flip
+
+coin = ["heads", "tails", "tails", "heads", "heads"]
+
+# Favorite performing artists
+
+artist = ["Rich Chigga", "joji", "Higher Brothers"]
+
+# Colors as symbols
+
+fav_symbols = [:red, :yellow, :purple, :blue]
+
+# Words and Definitions
+
+definitions = {:chop => "To mack on", :mack => "To chop", :shorty => "Girl"}
+
+definitions2 = { chop: "To mack on", mack: "To chop", shorty: "Girl"}
+
+# Movies
+
+fav_movies = {scott_pilgrim_vs_the_world: "2010", logan: "2017", wonder_woman: "2017"}
+
+# Cities
+
+gta = { markham: 330000, mississauga: 760000, brampton: 560000 }
+
+# Names and Age
+
+namage = { danny: 27, fon: 26, brandon: 23, charmaine: 18 }
