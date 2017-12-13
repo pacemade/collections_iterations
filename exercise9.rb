8cde6aeddb98caee377a9fb6933617574b8d5394
Danny Chang <dannychachang@gmail.com>
2017-12-13 18:01:26 -0500

    Exercise 10 (labeled 9?)

---
+++ exercise9.rb
@@ -0,0 +1,35 @@
+
+
+# 1
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+# 2
+def display(hash)
+  hash.each { |cohort, size| puts "#{cohort}: #{size}"}
+end
+
+#3
+students[:cohort4] = 43
+
+#4
+students.keys
+
+#5
+students = students.transform_values { |size| size * 1.05}
+
+# 6
+students.delete(:cohort2)
+
+# 7
+
+sum = 0
+
+students.each { |cohort, size| sum += size}
+
+sum
+
+# 8
