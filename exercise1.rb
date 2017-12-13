6792de505fd14fd537a18dd211eef0c2b9cf122e
Danny Chang <dannychachang@gmail.com>
2017-12-13 14:15:39 -0500

    Exercise 1, printing things out

--- exercise1.rb
+++ exercise1.rb
@@ -2,38 +2,69 @@
 
 # Colors
 
-fav_colors = ["red", "yellow", "purple", "blue"]
+# fav_colors = ["red", "yellow", "purple", "blue"]
+#
+# # Age
+#
+# age = [27, 23, 26, 18]
+#
+# # Coin flip
+#
+# coin = ["heads", "tails", "tails", "heads", "heads"]
+#
+# # Favorite performing artists
+#
+# artist = ["Rich Chigga", "joji", "Higher Brothers"]
+#
+# # Colors as symbols
+#
+# fav_symbols = [:red, :yellow, :purple, :blue]
+#
+# # Words and Definitions
+#
+# definitions = {:chop => "To mack on", :mack => "To chop", :shorty => "Girl"}
+#
+# definitions2 = { chop: "To mack on", mack: "To chop", shorty: "Girl"}
+#
+# # Movies
+#
+# fav_movies = {scott_pilgrim_vs_the_world: "2010", logan: "2017", wonder_woman: "2017"}
+#
+# # Cities
+#
+# gta = { markham: 330000, mississauga: 760000, brampton: 560000 }
+#
+# # Names and Age
+#
+# namage = { danny: 27, fon: 26, brandon: 23, charmaine: 18 }
 
-# Age
 
-age = [27, 23, 26, 18]
+# Exercise 1 ------------------------------------------------------------
 
-# Coin flip
+# Print coinflip
 
-coin = ["heads", "tails", "tails", "heads", "heads"]
+coinflip = ["heads", "tails", "tails", "heads", "heads"]
 
-# Favorite performing artists
+coinflip
 
-artist = ["Rich Chigga", "joji", "Higher Brothers"]
+# First Colors
 
-# Colors as symbols
+fav_colorsp = ["red", "yellow", "purple", "blue"]
 
-fav_symbols = [:red, :yellow, :purple, :blue]
+fav_colorsp[0]
 
-# Words and Definitions
+# Sorted ages
 
-definitions = {:chop => "To mack on", :mack => "To chop", :shorty => "Girl"}
+ages = [27, 23, 26, 18]
 
-definitions2 = { chop: "To mack on", mack: "To chop", shorty: "Girl"}
+ages.sort
 
-# Movies
+# Add a baby
 
-fav_movies = {scott_pilgrim_vs_the_world: "2010", logan: "2017", wonder_woman: "2017"}
+ages << 0
 
-# Cities
+# Movie date
 
-gta = { markham: 330000, mississauga: 760000, brampton: 560000 }
+movies = {scott_pilgrim_vs_the_world: "2010", logan: "2017", wonder_woman: "2017"}
 
-# Names and Age
-
-namage = { danny: 27, fon: 26, brandon: 23, charmaine: 18 }
+movies[:logan]
