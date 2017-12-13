dac6f41938bd584327d3d5e4c15f100f46b70fec
Danny Chang <dannychachang@gmail.com>
2017-12-13 18:32:41 -0500

    Fix Exercise 10 to display Bitmakers

--- exercise10.rb
+++ exercise10.rb
@@ -2,12 +2,12 @@
 count = (1..100).to_a
 
 count.each { |num|
-  if num % 3 == 0
+  if num % 3 ==0 && num % 5 == 0
+    puts "Bitmaker"
+    elsif num % 3 == 0
       puts "Bit"
     elsif num % 5 == 0
       puts "Maker"
-    elsif num % 3 ==0 && num % 5 == 0
-      puts "Bitmaker"
     else
       puts num
   end
---
+++ pizza.rb
@@ -0,0 +1,8 @@
+# pizzamaker
+
+puts "How many pizzas would you like?"
+
+quantity = gets.to_i
+
+until quantity == 0
+puts "How many toppings for pizza #{quantity}?"
